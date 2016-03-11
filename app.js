46e0033a3c6a6f7f10ac29b48fb33402162670dd
Dashkevich Andrew <andrew.dashkevich@yahoo.com>
2016-03-11 16:29:28 +0300

    add auth token support add jade index page

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ var baucis = require('baucis');
 
 var mongoose = require('mongoose');
 
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan');
 
+var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var User   = require('./app/model/user'); // get our mongoose model
+
+var index = require('./routes/index');
 // start mongoose
 mongoose.connect('mongodb://127.0.0.1:27017/scratch_map');
 var db = mongoose.connection;
@@ -33,6 +39,19 @@ db.once('open', function callback () {
         app.set('views', __dirname + '../app/scripts/views');
     });*/
 
+    // view engine setup
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'jade');
+
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+    app.use(morgan('dev'));
+
+
+    app.use('/', index);
+
     app.use('/api/v1', baucis());
 
     // simple log
@@ -42,79 +61,123 @@ db.once('open', function callback () {
     });
 
     // route index.html
-    app.get('/', function(req, res){
+    /*app.get('/', function(req, res){
         res.sendfile( path.join( __dirname, '../app/index.html' ) );
+    });*/
+
+    app.get('/setup', function(req, res) {
+
+        // create a sample user
+        var nick = new User({
+            name: 'Nick Cerminara',
+            password: 'password',
+            admin: true
+        });
+
+        // save the sample user
+        nick.save(function(err) {
+            if (err) throw err;
+
+            console.log('User saved successfully');
+            res.json({ success: true });
+        });
     });
+
+    // API ROUTES -------------------
+
+    // get an instance of the router for api routes
+    var apiRoutes = express.Router();
+
+    // route to authenticate a user (POST http://localhost:8080/api/authenticate)
+    apiRoutes.post('/authenticate', function(req, res) {
+
+        // find the user
+        User.findOne({
+            name: req.body.name
+        }, function(err, user) {
+
+            if (err){
+                throw err;
+            }
+
+            if (!user) {
+                res.json({ success: false, message: 'Authentication failed. User not found.' });
+            } else if (user) {
+
+                // check if password matches
+                if (user.password != req.body.password) {
+                    res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+                } else {
+
+                    // if user is found and password is right
+                    // create a token
+                    var token = jwt.sign(user, app.get('superSecret') || 'ilovescotchyscotch', {
+                        expiresIn: 86400 // expires in 24 hours
+                    });
+
+                    // return the information including token as JSON
+                    res.json({
+                        success: true,
+                        message: 'Enjoy your token!',
+                        token: token
+                    });
+                }
+
+            }
+
+        });
+    });
+
+    //route middleware to verify a token
+    apiRoutes.use(function(req, res, next) {
+
+        // check header or url parameters or post parameters for token
+        var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+        // decode token
+        if (token) {
+
+            // verifies secret and checks exp
+            jwt.verify(token, app.get('superSecret') || 'ilovescotchyscotch', function(err, decoded) {
+                if (err) {
+                    return res.json({ success: false, message: 'Failed to authenticate token.' });
+                } else {
+                    // if everything is good, save to request for use in other routes
+                    req.decoded = decoded;
+                    next();
+                }
+            });
+
+        } else {
+
+            // if there is no token
+            // return an error
+            return res.status(403).send({
+                success: false,
+                message: 'No token provided.'
+            });
+
+        }
+    });
+
+    // route to show a random message (GET http://localhost:8080/api/)
+    apiRoutes.get('/', function(req, res) {
+        res.json({ message: 'Welcome to the coolest API on earth!' });
+    });
+
+    // route to return all users (GET http://localhost:8080/api/users)
+    apiRoutes.get('/users', function(req, res) {
+        User.find({}, function(err, users) {
+            res.json(users);
+        });
+    });
+
+
+    // apply the routes to our application with the prefix /api
+    app.use('/api', apiRoutes);
 
     // start server
     http.createServer(app).listen(3000, function(){
         console.log('Express App started!');
     });
 });
-
-
-
-
-/*
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(require('less-middleware')(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-module.exports = app;
-*/
---
+++ public/js/test.js
@@ -0,0 +1,1 @@
+alert('all work correct!!!');
